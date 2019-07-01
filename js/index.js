c2cf2fff850bea144b39dad2d3e077035e91c578
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-01 17:36:42 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -11,3 +11,15 @@ const input = document.querySelector('input');
 input.addEventListener('keydown', function(e) {
   console.log(e.which);
 });
+
+
+divs = document.querySelectorAll('div');
+
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  // set the third argument to `true`!
+  divs[i].addEventListener('click', capture, true);
+}
