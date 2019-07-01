6c4fb6402a1db83d65e107b4afa117f3d7d3766b
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-01 17:29:49 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
-const main = document.getElementById('main');
-
-main.addEventListener('click', function(event) {
-  console.log('I was clicked!');
-});
+// const main = document.getElementById('main');
+//
+// main.addEventListener('click', function(event) {
+//   console.log('I was clicked!');
+// });
 
 const input = document.querySelector('input');
 
